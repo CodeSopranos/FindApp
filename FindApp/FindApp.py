cf8c56d9edab141261326fab2c398040e84e5719
Vadik Alperovich <vadikalperovich@mail.ru>
2019-12-06 02:31:56 +0300

    visual improvments with toolbar +  test execSQL() in TableLayout

--- FindApp/FindApp.py
+++ FindApp/FindApp.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import (QMainWindow,QWidget, QLabel, QLineEdit,  QPushButton,
-                             QApplication, QMessageBox,QAction,QGridLayout,
-                             QTableWidget, QTableWidgetItem,QInputDialog)
-from PyQt5.QtCore import Qt,QCoreApplication,QRect, QTimer
+							 QApplication, QMessageBox,QAction,QGridLayout,
+							 QTableWidget, QTableWidgetItem,QInputDialog)
+from PyQt5.QtCore import Qt,QCoreApplication,QRect, QTimer ,QSize
 from PyQt5.QtGui import QPainter, QColor, QFont,QIcon
 from PyQt5.Qt import QVBoxLayout
 
@@ -22,137 +22,158 @@ os.chdir(path)
 
 class FindApp(QMainWindow):
 
-    """
+	"""
 
-    class FindApp main layout of application
+	class FindApp main layout of application
 
-    """
+	"""
 
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-        TG=TextGetter.TextGetter()
-        self.textAbout=TG.get_textAbout()
-        self.AboutCounter=1
-        self.timer_itr=0
-        self.initUI()
+		TG=TextGetter.TextGetter()
+		self.textAbout=TG.get_textAbout()
+		self.AboutCounter=1
+		self.timer_itr=0
+		self.initUI()
 
-    def initUI(self):
+	def initUI(self):
 
-        """
+		"""
 
-        @method initUI creating main layput of application
+		@method initUI creating main layput of application
 
-        """
+		"""
 
-        self.setWindowTitle('FindApp')
-        self.setWindowIcon(QIcon('image/logo.png'))
-
-
-        """setting the about text"""
-        self.label_result = QLabel('',self)
-        self.label_result.setText(self.textAbout[0])
-        self.label_result.resize(700, 400)
-        self.label_result.move(0, 10)
-        self.label_result.setAlignment(Qt.AlignCenter)
-        self.label_result.setWordWrap(True)
-        font = QFont("Ubuntu Mono", 16)
-        self.label_result.setFont(font)
-
-        """text timer"""
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.tick)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.label_result)
-        self.setLayout(layout)
+		self.setWindowTitle('FindApp')
+		self.setWindowIcon(QIcon('image/logo.png'))
 
 
-        """QUIT toolbar botton"""
-        exitAction = QAction(QIcon('image/quit.png'), 'Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(self.close)
+		"""setting the about text"""
+		self.label_result = QLabel('',self)
+		self.label_result.setText(self.textAbout[0])
+		self.label_result.resize(700, 400)
+		self.label_result.move(0, 10)
+		self.label_result.setAlignment(Qt.AlignCenter)
+		self.label_result.setWordWrap(True)
+		font = QFont("Ubuntu Mono", 16)
+		self.label_result.setFont(font)
 
-        """View DB toolbar botton"""
-        viewDBAction = QAction(QIcon('image/database.png'), 'View Database', self)
-        viewDBAction.setShortcut('Ctrl+T')
-        viewDBAction.setStatusTip('View Database')
-        viewDBAction.triggered.connect(self.viewDBBtn)
+		"""text timer"""
+		self.timer = QTimer()
+		self.timer.timeout.connect(self.tick)
 
-        """Find Rebel toolbar botton"""
-        findAction = QAction(QIcon('image/find.png'), 'View Database', self)
-        findAction.setShortcut('Ctrl+F')
-        findAction.setStatusTip('Find Rebel')
-        findAction.triggered.connect(self.findAction)
+		layout = QVBoxLayout()
+		layout.addWidget(self.label_result)
+		self.setLayout(layout)
 
-        """INFO toolbar botton"""
-        infoAction = QAction(QIcon('image/info.png'), 'About', self)
-        infoAction.setShortcut('Ctrl+I')
-        infoAction.setStatusTip('About')
-        infoAction.triggered.connect(self.infoActionBtn)
+		"""Find Rebel toolbar botton"""
+		findAction = QAction(QIcon('image/find.png'), 'View Database', self)
+		findAction.setShortcut('Ctrl+F')
+		findAction.setStatusTip('Find Rebel')
+		findAction.triggered.connect(self.findAction)
 
-        """Set status bar """
-        self.statusBar()
+		"""View DB toolbar botton"""
+		viewDBAction = QAction(QIcon('image/database.png'), 'View Database', self)
+		viewDBAction.setShortcut('Ctrl+T')
+		viewDBAction.setStatusTip('View Database')
+		viewDBAction.triggered.connect(self.viewDBBtn)
 
-        """Set menubar"""
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAction)
-        fileMenu.addAction(viewDBAction)
+		"""INFO toolbar botton"""
+		infoAction = QAction(QIcon('image/info.png'), 'About', self)
+		infoAction.setShortcut('Ctrl+I')
+		infoAction.setStatusTip('About')
+		infoAction.triggered.connect(self.infoActionBtn)
 
-        """Set toolbar bottons"""
-        toolbar = self.addToolBar('Find Rebel')
-        toolbar.addAction(findAction)
+		"""HIDE toolbar botton"""
+		hideAction = QAction(QIcon('image/hide.png'), 'Hide toolbar', self)
+		hideAction.setShortcut('Ctrl+H')
+		hideAction.setStatusTip('Hide')
+		hideAction.triggered.connect(self.hideTbAction)
 
-        toolbar = self.addToolBar('View DB')
-        toolbar.addAction(viewDBAction)
+		"""SHOW toolbar botton"""
+		showAction = QAction(QIcon('image/show.png'), 'Show toolbar', self)
+		showAction.setShortcut('Ctrl+S')
+		showAction.setStatusTip('Show TB')
+		showAction.triggered.connect(self.showTbAction)
 
-        toolbar = self.addToolBar('About')
-        toolbar.addAction(infoAction)
-
-        toolbar = self.addToolBar('Exit')
-        toolbar.addAction(exitAction)
-
-        self.setGeometry(400, 100, 700, 450)
-        self.show()
-
-    def viewDBBtn(self):
-        reply = QMessageBox.question(self, 'View DB',
-            "Желаете посмотреть базу митингующих?", QMessageBox.Yes |
-             QMessageBox.Yes, QMessageBox.No)
-
-        if reply == QMessageBox.Yes:
-            self.close()
-            QWidget.__init__(self)
-            TableLayout.initUI(self)
-        else:
-            event.ignore()
-
-    """ launching timer for the about text"""
-    def infoActionBtn(self,event):
-        self.AboutCounter += 1
-        self.label_result.setText(self.textAbout[self.AboutCounter])
-        self.timer.start(2600)
-
-    """timer text trigger"""
-    def tick(self):
-        if self.AboutCounter == len(self.textAbout)-1:
-            self.label_result.setText(self.textAbout[self.AboutCounter])
-            self.AboutCounter = 1
-            self.timer.stop()
-        else:
-            self.AboutCounter += 1
-            self.label_result.setText(self.textAbout[self.AboutCounter])
+		"""QUIT toolbar botton"""
+		exitAction = QAction(QIcon('image/quit.png'), 'Exit', self)
+		exitAction.setShortcut('Ctrl+Q')
+		exitAction.setStatusTip('Exit application')
+		exitAction.triggered.connect(self.close)
 
 
+		"""Set status bar """
+		self.statusBar()
 
-    def findAction(self,event):
-        text, ok = QInputDialog.getText(self, 'Find Rebel',
-            'Введите фамилию:')
+		"""Set menubar"""
+		menubar = self.menuBar()
+		fileMenu = menubar.addMenu('&File')
+		fileMenu.addAction(findAction)
+		fileMenu.addAction(viewDBAction)
+		fileMenu.addAction(infoAction)
+		fileMenu.addAction(showAction)
+		fileMenu.addAction(exitAction)
 
 
-    def backBtnAction(self):
-        self.close()
-        QWidget.__init__(self)
-        self.initUI()
+		"""Set toolbar bottons"""
+		self.toolbar = self.addToolBar('main')
+
+		self.toolbar.addAction(findAction)
+		self.toolbar.addAction(viewDBAction)
+		self.toolbar.addAction(infoAction)
+		self.toolbar.addAction(hideAction)
+		self.toolbar.addAction(exitAction)
+
+		self.toolbar.setToolButtonStyle( Qt.ToolButtonTextUnderIcon)
+		self.toolbar.setIconSize(QSize(40, 40))
+
+
+		self.setGeometry(400, 100, 700, 450)
+		self.show()
+
+	def viewDBBtn(self):
+		reply = QMessageBox.question(self, 'View DB',
+			"Желаете посмотреть базу митингующих?", QMessageBox.Yes |
+			 QMessageBox.Yes, QMessageBox.No)
+
+		if reply == QMessageBox.Yes:
+			self.close()
+			QWidget.__init__(self)
+			TableLayout.initUI(self)
+		else:
+			event.ignore()
+
+	""" launching timer for the about text"""
+	def infoActionBtn(self,event):
+		self.AboutCounter += 1
+		self.label_result.setText(self.textAbout[self.AboutCounter])
+		self.timer.start(2800)
+
+	"""timer text trigger"""
+	def tick(self):
+		if self.AboutCounter == len(self.textAbout)-1:
+			self.label_result.setText(self.textAbout[self.AboutCounter])
+			self.AboutCounter = 1
+			self.timer.stop()
+		else:
+			self.AboutCounter += 1
+			self.label_result.setText(self.textAbout[self.AboutCounter])
+
+
+
+	def findAction(self,event):
+		text, ok = QInputDialog.getText(self, 'Find Rebel',
+			'Введите фамилию:')
+
+	def hideTbAction(self,event):
+		self.toolbar.setHidden(True)
+
+	def showTbAction(self,event):
+		self.toolbar.setHidden(False)
+
+	def backBtnAction(self):
+		self.close()
+		QWidget.__init__(self)
+		self.initUI()
--- FindApp/TableLayout.py
+++ FindApp/TableLayout.py
@@ -1,15 +1,33 @@
 from PyQt5.QtWidgets import (QMainWindow,QWidget, QLabel, QLineEdit,  QPushButton,
                              QApplication, QMessageBox,QAction,QGridLayout,
-                             QTableWidget, QTableWidgetItem)
-from PyQt5.QtCore import Qt,QCoreApplication,QRect
+                             QTableWidget, QTableWidgetItem, QToolBar)
+from PyQt5.QtCore import Qt,QCoreApplication,QRect,QSize
 from PyQt5.QtGui import QPainter, QColor, QFont,QIcon
 
 import sys
+import psycopg2
+
+
+def execSQL():
+
+    """
+
+    execute SQL query
+
+    """
+    conn = psycopg2.connect(dbname='postgres', user='postgres', password='36512', host='localhost')
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    cursor.execute("select * from ПОКУПКА")
+    frame = cursor.fetchall()
+    features=[feature[0] for feature in cursor.description]
+    return frame, features
 
 def initUI(self):
 
     """
-    
+
 	@function initUi creating Table Layout
 
 	"""
@@ -22,10 +40,15 @@ def initUI(self):
     backAction.setStatusTip('back')
     backAction.triggered.connect(self.backBtnAction)
 
+    """setting status bar"""
     self.statusBar()
-    toolbar = self.addToolBar('Back')
-    toolbar.addAction(backAction)
 
+    """setting toolbar"""
+    toolbar = QToolBar('main')
+    self.addToolBar(Qt.RightToolBarArea,toolbar)
+    toolbar.addAction(backAction)
+    toolbar.setToolButtonStyle( Qt.ToolButtonTextUnderIcon)
+    toolbar.setIconSize(QSize(40, 40))
 
     central_widget = QWidget(self)
     self.setCentralWidget(central_widget)
@@ -33,13 +56,21 @@ def initUI(self):
     grid_layout = QGridLayout()
     central_widget.setLayout(grid_layout)
 
+    frame,features=execSQL()
+
     table = QTableWidget(self)
-    table.setColumnCount(5)
-    table.setRowCount(10)
-    table.setHorizontalHeaderLabels(['X1','X2','X3','X4','X5'])
+    table.setColumnCount(len(frame[0]))
+    table.setRowCount(len(frame))
+    table.setHorizontalHeaderLabels(features)
     table.resizeColumnsToContents()
+    for i in range(len(frame)):
+        for j in range(len(frame[0])):
+            table.setItem(i, j, QTableWidgetItem(str(frame[i][j])))
 
     grid_layout.addWidget(table, 0, 0)
 
-    self.setGeometry(200, 100, 300, 500)
+
+    self.setGeometry(200, 100, 500, 400)
+    # self.adjustSize()
+
     self.show()
